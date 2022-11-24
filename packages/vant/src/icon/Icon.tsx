2c954ec9e98f221c7bfc2a0c4c780f33b4f09b07
suyulin <suyulingxm@hotmail.com>
2022-11-24 10:17:07 +0800

    feat(Icon): add icon support

--- packages/vant/src/icon/Icon.tsx
+++ packages/vant/src/icon/Icon.tsx
@@ -1,18 +1,19 @@
 import {
-  inject,
   computed,
   defineComponent,
-  type PropType,
   type ExtractPropTypes,
+  inject,
+  type PropType,
 } from 'vue';
 import {
   addUnit,
-  numericProp,
-  makeStringProp,
   createNamespace,
+  makeStringProp,
+  numericProp,
 } from '../utils';
 import { Badge, type BadgeProps } from '../badge';
 import { CONFIG_PROVIDER_KEY } from '../config-provider/ConfigProvider';
+import codeMap from './config';
 
 const [name, bem] = createNamespace('icon');
 
@@ -36,17 +37,18 @@ export default defineComponent({
 
   props: iconProps,
 
-  setup(props, { slots }) {
+  setup(props) {
     const config = inject(CONFIG_PROVIDER_KEY, null);
 
     const classPrefix = computed(
       () => props.classPrefix || config?.iconPrefix || bem()
     );
-
     return () => {
       const { tag, dot, name, size, badge, color } = props;
       const isImageIcon = isImage(name);
-
+      const renderCode = (iconName: any) => (
+        <span innerHTML={`&#x${codeMap[iconName].content}`}></span>
+      );
       return (
         <Badge
           dot={dot}
@@ -62,8 +64,11 @@ export default defineComponent({
           content={badge}
           {...props.badgeProps}
         >
-          {slots.default?.()}
-          {isImageIcon && <img class={bem('image')} src={name} />}
+          {isImageIcon ? (
+            <img class={bem('image')} src={name} />
+          ) : (
+            renderCode(`${classPrefix.value}-${name}`)
+          )}
         </Badge>
       );
     };
